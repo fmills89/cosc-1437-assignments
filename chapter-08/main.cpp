a29bb7de5bfc1a31a97573102e89267347625b65
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-09-09 17:07:28 -0500

    selectionSort working displaying array working

--- chapter-08/main.cpp
+++ chapter-08/main.cpp
@@ -12,41 +12,47 @@ using namespace std;
 
 // function prototypes - declaration of the func that tells the program about the tyoe of
 // val returned by the func
-void bubbleSort(int[], int);
-void selectionSort(int[], int);
+void displayArray(int[], int, string);
+void bubbleSort(int[], int, int &);
+void selectionSort(int[], int, int &);
 void swap(int &, int &);
 
 // marks beginning of a function
 int main()
 {
- const int SIZE = 6;
+  const int SIZE = 20;
+ 
+  int array_1[SIZE] = { 30, 20, 9, 28, 70, 29, 41, 52, 69, 18, 80, 51, 92, 32, 91, 38, 65, 46, 17, 16 };
 
- int nums[SIZE] = { 6, 1, 5, 7, 4, 8 };
+  int array_2[SIZE] = { 30, 20, 9, 28, 70, 29, 41, 52, 69, 18, 80, 51, 92, 32, 91, 38, 65, 46, 17, 16 };
+ 
 
- int values[SIZE] = { 6, 1, 5, 2, 4, 3 };
+int num_of_exchanges = 0;
 
+bubbleSort(array_1, SIZE, num_of_exchanges);
+displayArray(array_1, SIZE, "SORTED------|");
+cout << "\nNumber of exchanges = " << num_of_exchanges << endl;
 
-// sort the array
-bubbleSort(values, SIZE);
-
-selectionSort(nums, SIZE);
-
-// display the sorted array
-cout << "The sorted values: \n";
-for (auto element : values)
-  cout << element << " ";
-cout << endl;
-
-cout << "The sorted nums: \n";
-for (auto element : nums)
-  cout << element << " ";
-cout << endl;
+num_of_exchanges = 0;
+selectionSort(array_2, SIZE, num_of_exchanges);
+displayArray(array_2, SIZE, "SORTED------|");
+cout << "\nNumber of exchanges = " << num_of_exchanges << endl;
 
 return 0;
+
 }
 
+
+void displayArray(int array[], int SIZE, string prompt) {
+  cout << prompt << endl;
+  for (int i = 0; i < SIZE; i++)
+    cout << array[i] << ", ";
+  cout << endl;
+}
+
+
 // bubblesort in acensding order
-void bubbleSort(int array[], int size) {
+void bubbleSort(int array[], int size, int &num_of_exchanges) {
   int maxElement;
   int index;
 
@@ -54,12 +60,14 @@ void bubbleSort(int array[], int size) {
     for (index = 0; index < maxElement; index++) {
       if (array[index] > array[index + 1]) {
         swap(array[index], array[index + 1]);
+        num_of_exchanges++;
       }
     }
   };
 }
+
  // selectionSort in ascending order
-void selectionSort(int array[], int size)
+void selectionSort(int array[], int size, int&(num_of_exchanges))
 {
   int minIndex, minValue;
 
@@ -73,13 +81,12 @@ void selectionSort(int array[], int size)
       {
         minValue = array[index];
         minIndex = index;
+        num_of_exchanges++;
       }
     }
     swap(array[minIndex], array[start]);
   }
 }
-
-
 
 // swap func swaps a and b in memory
 void swap(int &a, int &b) {
